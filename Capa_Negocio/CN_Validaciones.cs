4f2ba53565c29c13cb6b05a4850cfa6ba0fe2e75
Francis Gonzalez <Kitomc.rd@gmail.com>
2020-03-15 15:22:55 -0400

    Metodos de validaion de numeros insertado

---
+++ Capa_Negocio/CN_Validaciones.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Capa_Negocio
+{
+   static public class CN_Validaciones
+    {
+
+        //Solo numero Validacion
+
+        public static bool SoloNumero(TextBox  T1)
+    {
+            MessageBox.Show("Solo numeros admitidos");
+             return true;
+    }
+        public static bool SoloNumero(TextBox T1, TextBox T2)
+        {
+            MessageBox.Show("Solo numeros admitidos");
+            return true;
+        }
+        public static bool SoloNumero(TextBox T1, TextBox T2, TextBox T3)
+        {
+            MessageBox.Show("Solo numeros admitidos");
+            return true;
+        }
+
+
+
+
+    }
+
+}
