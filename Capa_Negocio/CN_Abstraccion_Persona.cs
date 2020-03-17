e49fd2810c55a46b35139b1367fc72347ea5f10d
Francis Gonzalez <Kitomc.rd@gmail.com>
2020-03-17 11:13:58 -0400

    Actualizacion Clase Abstracta

--- Capa_Negocio/CN_Abstraccion_Persona.cs
+++ Capa_Negocio/CN_Abstraccion_Persona.cs
@@ -17,7 +17,12 @@ namespace Capa_Negocio
         public string Apellido { get => _apellido; set => _apellido = value; }
         public string Telefono { get => _telefono; set => _telefono = value; }
 
-       
+        public abstract bool InsertarEnDB();
+        public abstract bool BorrarEnDB();
+        public abstract bool ActualizarEnDB();
+        public abstract bool list<LeerEnDB>();
+
+
 
 
 
